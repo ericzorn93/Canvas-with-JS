c132d699becc0a00f79171e7a5baa4171a08ba52
Eric Zorn <goldencatch93@gmail.com>
2017-07-18 15:48:51 -0400

    Second Commit

--- js/rectangle.js
+++ js/rectangle.js
@@ -1,35 +1,49 @@
 /* Eric Zorn: July 15, 2017, Module 6 */
- 
-var canvas = document.getElementById('myCanvas'); 
-var context = canvas.getContext('2d');
 
-var canvasHeight = canvas.height = window.innerHeight - 200;
- var canvasWidth = canvas.width = window.innerWidth - 20;
+//Assurance that the document it loaded
+window.document.onload = console.log('The document is loaded!');
+
+//Canvas and Context Globals
+var canvas = window.document.getElementById('myCanvas');
+var ctx = canvas.getContext('2d');
+var mousePressed = false;
 
 
-function drawArea(wid, hgt) {
+
+//Resizing the Canvas
+document.getElementById('myCanvas').width = innerWidth - 25;
+document.getElementById('myCanvas').height = innerHeight - 25;
+
+
+
+function fillForm(wid, hgt) {
     "use strict";
-    var x = Math.random() * innerWidth;
-    var y = Math.random() * innerHeight;
-    context.clearRect(0,0, canvas.width, canvas.height);
-    context.fillStyle = "#ffff00";
-    context.fillRect(x, y, wid, hgt);
+    document.getElementById('recWid').value = wid;
+    document.getElementById('recHgt').value = hgt;
+    document.getElementById('areaOutput').value = wid * hgt;
+    document.getElementById('perimOutput').value = (wid * 2) + (hgt * 2);
+
+    return wid && hgt;
 }
 
-function mouseClick() {
-    //Code goes here...
-    
+function getEvent() {
+    "use strict";
+    var mouseDown = context.mousePressed;
 }
 
+
+ 
+
+
+
+//Initializing Function
 function init() {
-    drawArea(45,24);
-    mouseClick();
+    fillForm(33,30);
+    getEvent();
 }
 
 
+
+
+//Load the Init Function
 window.document.onload = init();
-window.document.onload = canvasHeight;
-window.document.onload = canvasWidth;
-
-
-
